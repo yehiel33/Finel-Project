512814b0de10a597ef09943e785fda0a378780c0
omer okun <doubleomurfy@gmail.com>
2020-05-31 11:07:25 +0300

    יצירת משתמש

--- Finel Project/Guest List.cs
+++ Finel Project/Guest List.cs
@@ -24,5 +24,28 @@ namespace Finel_Project
             EventSeatingManager eventseatingmanager = new EventSeatingManager();
             eventseatingmanager.ShowDialog();
         }
+
+        private void gUEST_LISTBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.gUEST_LISTBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.finel_ProjectDataSet);
+
+        }
+
+        private void gUEST_LISTBindingNavigatorSaveItem_Click_1(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.gUEST_LISTBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.finel_ProjectDataSet);
+
+        }
+
+        private void GuestList_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'finel_ProjectDataSet.GUEST_LIST' table. You can move, or remove it, as needed.
+            this.gUEST_LISTTableAdapter.Fill(this.finel_ProjectDataSet.GUEST_LIST);
+
+        }
     }
 }
