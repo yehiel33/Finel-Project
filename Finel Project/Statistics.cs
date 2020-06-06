8b96dafb20692a448861aeb843b747eca590ca1d
omer okun <doubleomurfy@gmail.com>
2020-06-06 18:03:46 +0300

    1

--- Finel Project/Statistics.cs
+++ Finel Project/Statistics.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.OleDb;
+using System.Data.OleDb; //Access
 
 namespace Finel_Project
 {
