44390688c3db05f4d74fb51009c876982174ba63
omer okun <doubleomurfy@gmail.com>
2020-06-06 17:14:29 +0300

    3

--- Finel Project/Statistics.Designer.cs
+++ Finel Project/Statistics.Designer.cs
@@ -30,6 +30,10 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.btnBack = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblRegister = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -52,11 +56,59 @@
             this.btnBack.UseVisualStyleBackColor = false;
             this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.DarkCyan;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label4.Location = new System.Drawing.Point(291, 228);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(252, 27);
+            this.label4.TabIndex = 63;
+            this.label4.Text = "Dish Type";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.DarkCyan;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label3.Location = new System.Drawing.Point(33, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(252, 27);
+            this.label3.TabIndex = 62;
+            this.label3.Text = "Age";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.DarkCyan;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label2.Location = new System.Drawing.Point(291, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(252, 27);
+            this.label2.TabIndex = 61;
+            this.label2.Text = "Sides";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblRegister
+            // 
+            this.lblRegister.BackColor = System.Drawing.Color.DarkCyan;
+            this.lblRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.lblRegister.Location = new System.Drawing.Point(33, 26);
+            this.lblRegister.Name = "lblRegister";
+            this.lblRegister.Size = new System.Drawing.Size(252, 27);
+            this.lblRegister.TabIndex = 60;
+            this.lblRegister.Text = "Male / Female";
+            this.lblRegister.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // Statistics
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 553);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblRegister);
             this.Controls.Add(this.btnBack);
             this.Controls.Add(this.label1);
             this.Name = "Statistics";
@@ -69,5 +121,9 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblRegister;
     }
 }
