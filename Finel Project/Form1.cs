29fd3da6e627995b83453fe8d404f083b6b7a884
omer okun <doubleomurfy@gmail.com>
2020-06-06 17:13:31 +0300

    2

---
+++ Finel Project/Form1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Finel_Project
+{
+	public partial class Form1 : Finel_Project.Statistics
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+	}
+}
